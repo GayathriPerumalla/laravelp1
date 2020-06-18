9bdefd95fcf3e9a7c4e79e948a64728e7ec4a445
GayathriPerumalla <66305898+GayathriPerumalla@users.noreply.github.com>
2020-06-18 16:29:54 +0530

    Add files via upload

---
+++ create.blade.php
@@ -0,0 +1,7 @@
+<h1>Add New Customer</h1>
+<form  action="/">
+ <input type="text" name="name">
+ <input type="text" name="email">
+ <submit>AddNewCustomerr</submit>
+</form>
+ 
---
+++ show.blade.php
@@ -0,0 +1,6 @@
+<h1>Customer details</h1>
+<strong>name</strong>
+<p>{{ $customer->name}}</p>
+
+<strong>email</strong>
+<p>{{ $customer->email}}</p>
